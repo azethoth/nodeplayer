4bd78b322e7d64c75a7867a65152eae82e75fa68
David Yung <azethoth@gmail.com>
2011-03-20 11:38:15 -0700

    store userid in a cookie and display on addvids page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express.createServer();
 app.configure(function(){
   app.use(express.static(__dirname + '/files'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
 });
 app.register('.haml', require('hamljs'));
 app.set('view engine', 'haml');
@@ -18,28 +19,26 @@ app.get('/nodeplayer', function(req, res) {
 });
 
 app.get('/addvids', function(req, res) {
-  res.render('addvids');
+  res.render('addvids', {vids:vids, userid:req.cookies.userid});
 });
 
 app.get('/nextvideo', function(req, res) {
-  res.send(vids.length ? vids.shift() : '');
-});
-
-app.get('/listvids', function(req, res) {
-  var str = '';
-  for (var i = 0; i < vids.length; ++i) {
-    str += vids[i] + '<br/>';
-  }
-  res.send(str);
+  res.send(vids.length ? vids.shift().v : '');
 });
 
 app.post('/addvid', function(req, res) {
+  var userid = req.body.userid;
+  if (userid) {
+    res.cookie('userid', userid, {maxAge:1000*60*24*30, path:'/'});
+  } else {
+    userid = req.cookies.userid;
+  }
   var id = req.body.vidid;
   if (id) {
     var qs = id.substr(id.lastIndexOf('?')+1);
     var query = querystring.parse(qs);
     if (query.v) {
-      vids.push(query.v);
+      vids.push({v:query.v,u:userid});
     }
   }
   res.redirect('/addvids');
